5ce66b59730eb0aebb0792a31617d3f1c57909b5
Nathan Oliveira <nateoliveira.dev@gmail.com>
2022-08-03 10:21:20 -0300

    consertando o bug do cypress, requisito 7 completo

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ function firstColor() {
 }
 
 firstColor();
+
+function removeOrAddClass(element) {
+    const aimElement = element.target;
+    const catchClass = document.getElementsByClassName('selected');
+
+    for (let i = 0; i < catchClass.length; i++) {
+        catchClass[i].classList.remove('selected');
+    }
+    aimElement.classList.add('selected');
+}
+
+const chosenPalette = document.querySelector('#color-palette');
+chosenPalette.addEventListener('click', removeOrAddClass);
